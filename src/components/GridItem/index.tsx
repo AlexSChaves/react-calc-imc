ce9654875027c73c1f7291711ec816b21d0a7a24
AlexSChaves <alexsandro091092@gmail.com>
2022-07-04 02:35:47 -0300

    First Commit

---
+++ src/components/GridItem/index.tsx
@@ -0,0 +1,31 @@
+import { Level } from '../../helpers/imc';
+import styles from './style.module.css';
+import upImage from '../../assets/up.png';
+import downImage from '../../assets/down.png';
+
+type Props = {
+	item: Level;
+};
+
+const GridItem = ({ item }: Props) => {
+	return (
+		<div className={styles.main} style={{ backgroundColor: item.color }}>
+			<div className={styles.gridIcon}>
+				{item.icon === 'up' && <img src={upImage} alt='' width='30' />}
+				{item.icon === 'down' && <img src={downImage} alt='' width='30' />}
+			</div>
+			<div className={styles.gridTitle}>{item.title}</div>
+			{item.yourIMC && (
+				<div className={styles.yourIMC}>Seu IMC é de {item.yourIMC} kg/m²</div>
+			)}
+			<div className={styles.gridInfo}>
+				<>
+					IMC está entre <strong>{item.imc[0]}</strong> e{' '}
+					<strong>{item.imc[1]}</strong>
+				</>
+			</div>
+		</div>
+	);
+};
+
+export default GridItem;
